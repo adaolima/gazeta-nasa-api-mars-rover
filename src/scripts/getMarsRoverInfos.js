a92028d64c15de716a47c19498200119ee06197d
Adão Lima <adaoolima@gmail.com>
2020-03-10 14:17:51 -0300

    script to consume api

---
+++ src/scripts/getMarsRoverInfos.js
@@ -0,0 +1,10 @@
+const getMarsRoverInfos = (sol, page) => {
+  const URL_API_NASA = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}${page ? '&page='+ page: ''}&api_key=MrfBZ9661U9AdWtFle3BvcuIdWBcwcOexiRa6zFw`;
+  var dataset =fetch(URL_API_NASA)
+  .then(res=>res.json())
+  .then(res=>res.photos)
+  .catch(err=>err);
+  return dataset;
+} 
+
+export default getMarsRoverInfos;
