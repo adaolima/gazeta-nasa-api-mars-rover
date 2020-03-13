3bd59f061b837312a2a1f396f5e5183cdb35edd2
Adao Lima <adaoolima@gmail.com>
2020-03-13 12:33:14 -0300

    Prettier

--- src/scripts/buildCarousel.js
+++ src/scripts/buildCarousel.js
@@ -1,14 +1,19 @@
-import SlideItem from './SlideItem';
+import SlideItem from "./SlideItem";
 
-const buildCarousel = (data) => {
-  let slides = '';
+const buildCarousel = data => {
+  let slides = "";
   let indexesSlides = [];
-  data.map( (item, index) => {
-    slides += SlideItem(item.id, item.rover.name, item.earth_date, item.img_src, index);
+  data.map((item, index) => {
+    slides += SlideItem(
+      item.id,
+      item.rover.name,
+      item.earth_date,
+      item.img_src,
+      index
+    );
     indexesSlides.push(index);
   });
-  return { slides , indexesSlides };
-
+  return { slides, indexesSlides };
 };
 
 export default buildCarousel;
