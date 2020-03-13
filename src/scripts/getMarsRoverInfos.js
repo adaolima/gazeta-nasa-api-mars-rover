687313d1fa34edb3b90c448807d6cf9cce84089a
Adao Lima <adaoolima@gmail.com>
2020-03-13 12:33:41 -0300

    Added a console to test

--- src/scripts/getMarsRoverInfos.js
+++ src/scripts/getMarsRoverInfos.js
@@ -1,10 +1,13 @@
 const getMarsRoverInfos = (sol, page) => {
-  const URL_API_NASA = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}${page ? '&page='+ page: ''}&api_key=MrfBZ9661U9AdWtFle3BvcuIdWBcwcOexiRa6zFw`;
-  var dataset =fetch(URL_API_NASA)
-  .then(res=>res.json())
-  .then(res=>res.photos)
-  .catch(err=>err);
+  const URL_API_NASA = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}${
+    page ? "&page=" + page : ""
+  }&api_key=MrfBZ9661U9AdWtFle3BvcuIdWBcwcOexiRa6zFw`;
+  var dataset = fetch(URL_API_NASA)
+    .then(res => res.json())
+    .then(res => res.photos)
+    .catch(err => err);
+  console.log(dataset);
   return dataset;
-} 
+};
 
 export default getMarsRoverInfos;
