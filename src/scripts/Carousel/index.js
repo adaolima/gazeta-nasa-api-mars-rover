eac8858e577491dc5993ef1577aface64d1465ad
Adao Lima <adaoolima@gmail.com>
2020-03-13 12:34:07 -0300

    Simplify Carousel component

--- src/scripts/Carousel/index.js
+++ src/scripts/Carousel/index.js
@@ -2,28 +2,26 @@ import buildCarousel from "../buildCarousel";
 import getMarsRoverInfos from "../getMarsRoverInfos";
 
 const Carousel = (sol, page) => {
-  const carouselContainer = document.getElementById('carouselMarsControls');
-  const formSearcher =  document.getElementById('formSearchMarsInfos').querySelector('#inputNumber');
+  const carouselContainer = document.getElementById("carouselMarsControls");
+  const formSearcher = document
+    .getElementById("formSearchMarsInfos")
+    .querySelector("#inputNumber");
   formSearcher.value = sol;
-  
-   getMarsRoverInfos(sol, page).then((res)=> {
-    var { slides,  indexesSlides } = buildCarousel(res);
-    construct(slides, indexesSlides)
-   });
-  
- 
-  function construct(slides, indexesSlides) {
-    let dotNav = '';
-    indexesSlides.map((dot) => {
-      dotNav += `<li data-target="#carouselExampleIndicators" data-slide-to="${dot}" class="${dot == 0 ? 'active': ''}"></li>`
+
+  getMarsRoverInfos(sol, page)
+    .then(res => {
+      return buildCarousel(res);
+    })
+    .then(({ slides }) => {
+      carouselContainer.innerHTML = buildBody(slides);
+      // console.log(slides);
     });
+
+  function buildBody(slides) {
     const bodyCarousel = `
     <div class="carousel-inner">
       ${slides}
       </div>
-      <ol class="carousel-indicators">
-        ${dotNav}
-      </ol>
       <a class="carousel-control-prev" href="#carouselMarsControls" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Anterior</span>
@@ -34,11 +32,8 @@ const Carousel = (sol, page) => {
     </a>
       `;
 
-    carouselContainer.innerHTML = bodyCarousel;
+    return bodyCarousel;
   }
-    
-  // console.log(slides);
-  // 
 };
 
 export default Carousel;
