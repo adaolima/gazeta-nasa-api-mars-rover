836b179ebb3764d34d4e2d7a32e174202e8e55f6
Adao Lima <adaoolima@gmail.com>
2020-03-13 12:32:02 -0300

    Added to index the listener to main searcher

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import 'bootstrap';
-import sendSearchDatas from './scripts/sendSearchDatas';
+import "bootstrap";
+// import sendSearchDatas from "./scripts/sendSearchDatas";
+import getMarsRoverInfos from "./scripts/getMarsRoverInfos";
+import buildCarousel from "./scripts/buildCarousel";
+import Carousel from "./scripts/Carousel";
+console.log("Carregando scripts");
 
+// var inputSearchValue = document.getElementById("inputNumber").value;
+var pageCurrent = 1;
+const callCarousel = event => {
+  event.preventDefault();
+  var formSearcher = event.target.parentNode.parentNode;
+  var inputSearchValue = formSearcher.querySelector("#inputNumber").value;
+  const btnSearch = event.target.parentNode;
+  var carouselMarsControls = formSearcher.parentNode.parentNode.querySelector(
+    ".carousel"
+  );
+  if (!btnSearch.classList.contains("is-loading")) {
+    Carousel(inputSearchValue, pageCurrent);
 
+    btnSearch.classList.add("is-loading");
+    btnSearch.setAttribute("disabled", true);
+    console.log(carouselMarsControls);
+  }
+  console.log(inputSearchValue);
+  console.log(pageCurrent);
+};
 
-
+document.getElementById("btnLoad").addEventListener("click", callCarousel);
 
 // Import plugins individually
 /*
@@ -11,4 +34,4 @@ import 'bootstrap/js/dist/util';
 import 'bootstrap/js/dist/dropdown';
 */
 
-import './main.scss';
+import "./main.scss";
